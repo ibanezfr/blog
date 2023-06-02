ea0cffa988177bbb4abfc986ba764e4ee46dcc78
franco-ibanez-dev <francoibanez.dev@gmail.com>
2023-06-02 10:34:03 -0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,21 @@
-AOS.init();
+let prevScrollpos = window.pageYOffset || 0;
 
-// You can also pass an optional settings object
-// below listed default settings
 AOS.init({
-  
-  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-  offset: 120, // offset (in px) from the original trigger point
-  delay: 0, // values from 0 to 3000, with step 50ms
-  duration: 600, // values from 0 to 3000, with step 50ms
-  easing: 'ease', // default easing for AOS animations
-  once: false, // whether animation should happen only once - while scrolling down
-  mirror: false, // whether elements should animate out while scrolling past them
-  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
-
+  offset: 120,
+  delay: 0,
+  duration: 600,
+  easing: 'ease',
+  once: false,
+  mirror: false,
+  anchorPlacement: 'top-bottom',
 });
 
-
-// Back to top button:
 let mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+  toggleNavbar();
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -30,14 +25,22 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0; // Para Safari
+  document.documentElement.scrollTop = 0; // Para Chrome, Firefox, IE y Opera
+  toggleNavbar();
 }
 
+function toggleNavbar() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector('.navbar').classList.remove('hidden');
+  } else {
+    document.querySelector('.navbar').classList.add('hidden');
+  }
+  prevScrollpos = currentScrollPos;
+}
 
-// To collapse  the navigation menu after clicking an option
 function collapseNavbar() {
   const navbarCollapse = document.getElementById('navbarNav');
   const navbarToggle = document.querySelector('.navbar-toggler');
